5080a66c03e7eb7f2d69b12cbf9a6d3e5235626c
IdraDev <dragonfx1234@gmail.com>
2026-08-26 00:05:14 +0200

    fix(ui): loosen the leading on modal titles

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -151,7 +151,7 @@ const DialogTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      "text-lg leading-none font-semibold tracking-tight",
+      "text-lg leading-snug font-semibold tracking-tight",
       className,
     )}
     {...props}
